7bb01da04c574a5e5b18604736be3d635695d858
Brook_X <birukgelila309@gmail.com>
2023-10-13 14:45:56 +0300

    added google sign in method with the intenet provider to check the internet status

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import "dart:async";
+
 import "package:fire_auths/firebase_options.dart";
+import "package:fire_auths/provider/internet_provider.dart";
 import "package:fire_auths/provider/sign_in_provider.dart";
 import "package:fire_auths/screens/home_page.dart";
 import "package:fire_auths/screens/sign_in_page.dart";
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 import "package:provider/provider.dart";
 
 Future<void> main() async {
@@ -11,7 +15,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const MyApp());
 }
 
@@ -25,18 +28,68 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: SignInProvider(),
         ),
+        ChangeNotifierProvider.value(
+          value: InternetProvider(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: "/",
         routes: {
           "/": (context) {
-            final sp = context.read<SignInProvider>();
-
-            return sp.isSignedIn! ? const HomePage() : const SignInPage();
+            return const SplashScreen();
           }
         },
       ),
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    final sp = context.read<SignInProvider>();
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      sp.isSignedIn == false
+          ? Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const SignInPage()))
+          : Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Hero(
+          tag: "Logo",
+          child: Stack(
+            children: [
+              const Image(
+                image: AssetImage("assets/firebase.png"),
+              ),
+              Positioned(
+                bottom: -1,
+                right: 3,
+                child: SvgPicture.asset(
+                  "assets/lock.svg",
+                  // height: 32,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
